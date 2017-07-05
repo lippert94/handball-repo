4caab5ceb6fd84bb269110bbbe3b742d8d38ebfb
tanulo7@athenepraxis.local <tanulo7@athenepraxis.local>
2017-07-05 20:46:43 +0200

    player[] befejezése

---
+++ ConsoleApplication1/FileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    public class FileHandler
+    {
+        
+
+        using (var fileStream = new FileStream(@"\ConsoleApplication1\subs.txt", FileMode.Open))
+            {
+                    
+
+                }
+       
+         
+               
+        
+
+    }
+}
