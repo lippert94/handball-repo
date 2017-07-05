189dd06d763fc68340d331c54b557d7194d2dc33
kovatsnorbert@athenepraxispraxis.local <kovatsnorbert@athenepraxispraxis.local>
2017-07-05 18:33:15 +0200

    Subs

---
+++ ConsoleApplication1/Sub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Sub
+    {
+
+        public int Min { get; }
+        public int PlayerUp { get; }
+        public int PlayerSub { get; }
+
+    }
+}
